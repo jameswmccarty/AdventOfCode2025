00a6512fb297596a1518cb7cc1213642d59dd814
jameswmccarty <jameswmccarty@gmail.com>
2025-12-07 16:19:42 -0500

    Update day07.py

--- day07.py
+++ day07.py
@@ -2,11 +2,26 @@
 
 # Advent of Code 2025 Day 07
 
+memo = dict()
+splitters = set()
+max_y = None
+def splitter_total(pos):
+	if pos in memo:
+		return memo[pos]
+	x, y = pos
+	if y > max_y:
+		return 0
+	if (x, y+1) not in splitters:
+		memo[pos] = splitter_total((x, y+1))
+		return memo[pos]
+	left = splitter_total((x-1, y+1))
+	right = splitter_total((x+1, y+1))
+	memo[pos] = 1 + left + right
+	return memo[pos]
 
 if __name__ == "__main__":
 
 	split_count = 0
-	splitters = set()
 	start = None
 	with open("day07_input", "r") as infile:
 		y = 0
@@ -34,3 +49,4 @@ if __name__ == "__main__":
 		y += 1
 	print(split_count)
 
+	print(splitter_total(start)+1)
